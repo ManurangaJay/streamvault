39d2d6501bd7b713765217c1efbb9835608f7f66
Manuranga <manurangajayawardhana@gmail.com>
2026-04-24 15:52:48 +0530

    update: add retention policy for Kafka topics

--- command-service/src/main/java/com/streamvault/command_service/config/KafkaTopicConfig.java
+++ command-service/src/main/java/com/streamvault/command_service/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.streamvault.command_service.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,11 +9,14 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
+    private static final String RETENTION_7_DAYS = "604800000";
+
     @Bean
     public NewTopic transactionEventsTopic() {
         return TopicBuilder.name("transaction.events")
                 .partitions(3)
                 .replicas(1)
+                .config(TopicConfig.RETENTION_MS_CONFIG, RETENTION_7_DAYS)
                 .build();
     }
 
@@ -21,6 +25,7 @@ public class KafkaTopicConfig {
         return TopicBuilder.name("account.events")
                 .partitions(3)
                 .replicas(1)
+                .config(TopicConfig.RETENTION_MS_CONFIG, RETENTION_7_DAYS)
                 .build();
     }
 }
